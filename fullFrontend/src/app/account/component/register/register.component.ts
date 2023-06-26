ed2536586ba9eac7c7d9a7ee51b643cc4d4c04f9
novakblanka03 <novakblanka2003@gmail.com>
2023-06-26 11:44:44 +0300

    Some changes made to the register.component.ts

--- fullFrontend/src/app/account/component/register/register.component.ts
+++ fullFrontend/src/app/account/component/register/register.component.ts
@@ -1,70 +1,85 @@
-import { ActivatedRoute, Router } from '@angular/router';
-import { FormBuilder, Validators } from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {FormBuilder, Validators} from '@angular/forms';
 
-import { AccountService } from '../services/account.service';
+import {AccountService} from '../services/account.service';
 import {Component} from '@angular/core';
 import {style} from "@angular/animations";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css'],
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  form = this.formBuilder.group({
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
-    email: ['', Validators.required],
-    password: ['', Validators.required],
-    gender: ['', Validators.required],
-    admin: ['']
-  });
-  loading = false;
-  submitted = false;
+    form = this.formBuilder.group({
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        email: ['', Validators.required],
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required],
+        gender: ['', Validators.required],
+        admin: ['']
+    });
+    loading = false;
+    submitted = false;
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private route: ActivatedRoute,
-    private router: Router,
-    private accountService: AccountService
-  ) {
-    this.form.valueChanges.subscribe((value) => console.log(value));
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private accountService: AccountService
+    ) {
+        this.form.valueChanges.subscribe((value) => console.log(value));
 
-    this.f.password.valueChanges.subscribe((value) => console.log(value));
-  }
-
-  // convenience getter for easy access to form fields
-  get f() {
-    return this.form.controls;
-  }
-
-  onSubmit() {
-    this.submitted = true;
-
-    // Stop here if the form is invalid
-    if (this.form.invalid) {
-      return;
+        this.f.password.valueChanges.subscribe((value) => console.log(value));
     }
 
-    this.loading = true;
-    this.accountService.register(this.form.value as any).subscribe({
+    // convenience getter for easy access to form fields
+    get f() {
+        return this.form.controls;
+    }
 
-      next: () => {
-        // Reset loading state and navigate to the login page on success
-        this.loading = false;
-        let mess = document.getElementById("success");
-        mess?.setAttribute("style","display:block");
+    onSubmit() {
+        this.submitted = true;
+        // Stop here if the form is invalid
+        if (this.form.invalid) {
+            return;
+        }
 
-        this.router.navigate(['/login'], { relativeTo: this.route });
+        const password1 = this.form.value.password;
+        const confirmPassword = this.form.value.confirmPassword;
 
-      },
-      error: (error) => {
-        // Handle the error and reset loading state
-        this.loading = false;
-        // Add error handling logic here (e.g., display error message)
-        console.log("Loading error (register)");
-      },
-    });
-  }
+        let passA = document.getElementById("passwordAlert");
+        if (!this.verifyPasswords(password1, confirmPassword)) {
+            passA?.setAttribute("style", "display:inline");
+            // Passwords do not match, display an error message or perform necessary actions
+            return;
+        }else {
+            passA?.setAttribute("style", "display:none");
+        }
+        this.loading = true;
+        this.accountService.register(this.form.value as any).subscribe({
+            next: () => {
+                // Reset loading state and navigate to the login page on success
+                this.loading = false;
+                let mess = document.getElementById("success");
+                mess?.setAttribute("style", "display:inline");
+                this.router.navigate(['/login'], {relativeTo: this.route});
+            },
+            error: (error) => {
+                // Handle the error and reset loading state
+                this.loading = false;
+                // Add error handling logic here (e.g., display error message)
+                console.log("Loading error (register)");
+            }
+        });
+    }
+
+    verifyPasswords(password: string | null | undefined, confirmPassword: string | null | undefined): boolean {
+        if (password === confirmPassword){
+            return true;
+        }
+        return false;
+    }
 }
 
